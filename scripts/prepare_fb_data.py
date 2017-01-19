7539de30d60b82fb08eb8dd483f9786fc9371431
woniesong92 <hs459@cornell.edu>
2017-01-19 01:10:06 -0800

    Add encoding and change delimiter to tab instead of comma

--- scripts/prepare_fb_data.py
+++ scripts/prepare_fb_data.py
@@ -1,14 +1,17 @@
 import pdb
 import csv
 import sys
+import gzip
 
 """
 Process the historical data from FB groups to conform the
 chatbot-retrieval data format.
+To run: $python prepare_fb_data.py input_file_path output_file_path
 """
 
 # NOTE: we don't make use of num_likes at all
 def parse_info_from_line(line):
+  line = line.decode('utf-8')
   raw = line.split("\t")
   post = {'num_likes': raw[0], 'body': raw[1].rstrip()}
   raw_comments = raw[2:]
@@ -18,17 +21,20 @@ def parse_info_from_line(line):
   return {'post': post, 'comments': comments}
 
 def process_raw_data(filename, output):
-  with open(filename) as f1:
+  with gzip.open(filename) as f1:
     with open(output, 'w') as f2:
       fieldnames = ['Context', 'Utterance']
-      writer = csv.DictWriter(f2, fieldnames=fieldnames)
+      writer = csv.DictWriter(f2, fieldnames=fieldnames, delimiter='\t')
       writer.writeheader()
 
       # NOTE: if a post has many comments, we add multiple rows with the same post body
       for line in f1:
         info = parse_info_from_line(line)
         for comment in info['comments']:
-          writer.writerow({'Context': info['post']['body'], 'Utterance': comment['body']})
+          writer.writerow({
+            'Context': info['post']['body'].encode('utf-8'),
+            'Utterance': comment['body'].encode('utf-8')
+          })
 
 if __name__ == "__main__":
   print("Processing FB data: " + sys.argv[1])
